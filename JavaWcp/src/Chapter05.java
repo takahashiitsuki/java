9917c83ea6db4f5e79c60477cdf665df92a92dc8
takahashiitsuki <itakaha71@gmail.com>
2023-01-09 03:13:40 +0900

    [Finish]Chapter5.java

---
+++ JavaWcp/src/Chapter05.java
@@ -0,0 +1,21 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		long long1 = 1;
+		double double1 = 0.1;
+		boolean boolean1 = true;
+		System.out.println(long1);
+		System.out.println(double1);
+		System.out.println(boolean1);
+		
+		Float float1 = 0.1f;
+		Integer intger1 = 1;
+		Character character1 = 'あ';
+		System.out.println(float1);
+		System.out.println(intger1);
+		System.out.println(character1);
+		
+		int[] int1 = { 0, 1, 2, 3, 4};
+		System.out.println(int1[3]);
+	}
+}
