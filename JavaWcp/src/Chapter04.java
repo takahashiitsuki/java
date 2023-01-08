ed8e119bcac2b57da7cb842b19743a4eceb0e53b
takahashiitsuki <itakaha71@gmail.com>
2023-01-08 21:59:27 +0900

    [FInish]Chapter04.java

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str = "高橋樹";
+		System.out.println(str);
+		
+		str = "天上天下唯我独尊";
+		System.out.println(str);
+		
+		final int lucky = 129;
+		System.out.println(lucky);
+	}
+}
