b548ad11ed49c0ced15b30de3c6f1938a2807a4f
takahashiitsuki <itakaha71@gmail.com>
2023-01-12 23:23:02 +0900

    [Finish]java9

---
+++ JavaWcp/src/Human01.java
@@ -0,0 +1,17 @@
+
+public class Human01 {
+	public String name;
+	public int age;
+	
+	//引数なしのコンストラクタ
+	public Human01() {
+		name = "山田";
+		age = 20;
+	}
+	
+	//引数2つのコンストラクタ
+	public Human01(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+}
