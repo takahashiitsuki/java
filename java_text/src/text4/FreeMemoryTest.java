dca5ee729ed77aef476b22bda8e257d810d3b764
takahashiitsuki <itakaha71@gmail.com>
2023-02-01 15:44:39 +0900

    [FInish]text4_1

---
+++ java_text/src/text4/FreeMemoryTest.java
@@ -0,0 +1,18 @@
+package text4;
+
+class DataSet{
+	int[] data = new int[100];
+}
+
+public class FreeMemoryTest {
+	public static void main(String[] args) {
+		System.out.println("空きメモリサイズ:" + Runtime.getRuntime().freeMemory());
+		DataSet[] data = new DataSet[10000];
+		for(int i = 0; i < 10000; i++) {
+			data[i] = new DataSet();
+			if((i + 1) % 100 == 0) {
+				System.out.println("生成済みインスタンス数:" + (i + 1) + "空きメモリサイズ:" + Runtime.getRuntime().freeMemory());
+			}
+		}
+	}
+}
