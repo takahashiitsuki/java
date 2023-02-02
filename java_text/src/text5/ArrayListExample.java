a4336b51adbd36fbbfd33342d1aaace0654ae635
takahashiitsuki <itakaha71@gmail.com>
2023-02-03 02:58:58 +0900

    [FInish]text5_1

---
+++ java_text/src/text5/ArrayListExample.java
@@ -0,0 +1,24 @@
+package text5;
+
+import java.util.ArrayList;
+
+public class ArrayListExample {
+	public static void main(String[] args) {
+		ArrayList<String> months = new ArrayList<String>();
+		
+		months.add("January");
+		months.add("February");
+		months.add("march");
+		
+		System.out.println("要素数 " + months.size());
+		for(int i = 0; i < months.size(); i++) {
+			System.out.println(months.get(i));
+		}
+		
+		months.remove(1);
+		System.out.println("要素数 " + months.size());
+		for(int i = 0; i < months.size(); i++) {
+			System.out.println(months.get(i));
+		}
+	}
+}
