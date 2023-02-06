6093a71c5fa59b1577d8f93891250e48b743d7cf
takahashiitsuki <itakaha71@gmail.com>
2023-02-07 08:42:58 +0900

    [FInish]text5_3

---
+++ java_text/src/text5/EnhancedForExample.java
@@ -0,0 +1,11 @@
+package text5;
+
+public class EnhancedForExample {
+	public static void main(String[] args) {
+		String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun" };
+		for(String str : months) {
+			System.out.println(str);
+		}
+	}
+
+}
