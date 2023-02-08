764eb681c230851b630c8f7218329c2e21c14659
takahashiitsuki <itakaha71@gmail.com>
2023-02-09 07:55:20 +0900

    [FInish]text6_1

---
+++ java_text_2/src/text6/InnerClassExample.java
@@ -0,0 +1,24 @@
+package text6;
+
+class Outer{
+	private String message = "Outerクラスのprivateなインスタンス変数です";
+	
+	void doSomething() {
+		class Inner{
+			void print() {
+				System.out.println("Innerクラスのprintメソッドが呼ばれました");
+				System.out.println(message);
+			}
+		}
+		
+		Inner inner = new Inner();
+		inner.print();
+	}
+}
+
+public class InnerClassExample {
+	public static void main(String[] args) {
+		Outer outer = new Outer();
+		outer.doSomething();
+	}
+}
