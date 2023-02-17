8cab6a5c0a3bae114d5a4841b76be97e54d5324e
takahashiitsuki <itakaha71@gmail.com>
2023-02-17 17:08:53 +0900

    [FInish]text7_4

--- java_text_2/src/text7/FileReadExample.java
+++ java_text_2/src/text7/FileReadExample.java
@@ -1,5 +1,23 @@
 package text7;
 
-public class FileReadExample {
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
+public class FileReadExample {
+	public static void main(String[] args) {
+		try {
+			File file = new File("C:/java/test.txt");
+			FileReader fr = new FileReader(file);
+			BufferedReader br = new BufferedReader(fr);
+			String s;
+			while((s = br.readLine()) != null) {
+				System.out.println(s + "を読み込みました");
+			}
+			br.close();
+		} catch(IOException e) {
+			System.out.println(e);
+		}
+	}
 }
