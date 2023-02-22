e81cf9d405b4cade7e0808c6089a83e9d130b414
takahashiitsuki <itakaha71@gmail.com>
2023-02-23 01:03:49 +0900

    [FInish]text8

---
+++ java_text_2/src/text8/SimpleApplication.java
@@ -0,0 +1,17 @@
+package text8;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SimpleApplication extends Application {
+	public void start(Stage stage) {
+		stage.setTitle("JavaFX Rxample");
+		stage.setWidth(300);
+		stage.setHeight(200);
+		stage.show();
+	}
+	
+	public static void main(String[] args) {
+		launch();
+	}
+}
