0d8d9b459c03c7bc0ae49b5c514001f08b8d0e15
takahashiitsuki <itakaha71@gmail.com>
2023-02-24 03:25:25 +0900

    [FInish]selflearn_java_1

---
+++ dokusyuu_java/src/selflearn_java/chap01/Hello.java
@@ -0,0 +1,10 @@
+package selflearn_java.chap01;
+
+import java.time.LocalDateTime;
+
+public class Hello {
+	public static void main(String[] args) {
+		LocalDateTime time = LocalDateTime.now();
+		System.out.println(time);
+	}
+}
