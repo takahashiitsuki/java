f1a2e60b713bb2dc922530b18c6b3f13ee0854fc
takahashiitsuki <itakaha71@gmail.com>
2023-02-27 02:21:59 +0900

    [FInish]Practice3_2

---
+++ dokusyuu_java/src/chap3/Practice3_2.java
@@ -0,0 +1,12 @@
+package chap3;
+
+public class Practice3_2 {
+	public static void main(String[] args) {
+		String value = null;
+		if(value == null) {
+			System.out.println("値なし");
+		}else {
+			System.out.println(value);
+		}
+	}
+}
