2493842b0f97758a0c271df0c323105bfff28b5c
takahashiitsuki <itakaha71@gmail.com>
2023-02-26 22:46:43 +0900

    [FInish]1_Practice3

--- dokusyuu_java/src/chap02/Practice1.java
+++ dokusyuu_java/src/chap02/Practice1.java
@@ -1,4 +1,4 @@
-package selflearn_java.chap01;
+package chap02;
 
 public class Practice1 {
 	public static void main(String[] args) {
---
+++ dokusyuu_java/src/chap02/Practice3.java
@@ -0,0 +1,10 @@
+package chap02;
+
+public class Practice3 {
+	public static void main(String[] args) {
+		double DISCOUNT = 0.9;
+		var price = 500;
+		var sum = price * DISCOUNT;
+		System.out.println((int)sum);
+	}
+}
