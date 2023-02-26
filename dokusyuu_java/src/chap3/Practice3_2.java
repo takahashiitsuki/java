e08ddc34462d6ea3ea97a3c0f25193a11ffd3232
takahashiitsuki <itakaha71@gmail.com>
2023-02-27 02:23:16 +0900

    [Fix]Practice3_2

--- dokusyuu_java/src/chap3/Practice3_2.java
+++ dokusyuu_java/src/chap3/Practice3_2.java
@@ -3,10 +3,6 @@ package chap3;
 public class Practice3_2 {
 	public static void main(String[] args) {
 		String value = null;
-		if(value == null) {
-			System.out.println("値なし");
-		}else {
-			System.out.println(value);
-		}
+		System.out.println(value == null?"値なし":value);
 	}
 }
