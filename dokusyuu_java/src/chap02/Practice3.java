4e1dc1a6049789c764ba6e9f4c79730c29345712
takahashiitsuki <itakaha71@gmail.com>
2023-02-26 22:48:53 +0900

    [Fix]Practice3

--- dokusyuu_java/src/chap02/Practice3.java
+++ dokusyuu_java/src/chap02/Practice3.java
@@ -2,7 +2,7 @@ package chap02;
 
 public class Practice3 {
 	public static void main(String[] args) {
-		double DISCOUNT = 0.9;
+		final double DISCOUNT = 0.9;
 		var price = 500;
 		var sum = price * DISCOUNT;
 		System.out.println((int)sum);
