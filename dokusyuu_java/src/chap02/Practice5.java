669a6b9b55e024ecca904b0592e65b30f5eac02f
takahashiitsuki <itakaha71@gmail.com>
2023-02-26 23:04:45 +0900

    [FInish]chap2

---
+++ dokusyuu_java/src/chap02/Practice5.java
@@ -0,0 +1,21 @@
+package chap02;
+
+public class Practice5 {
+	public static void main(String[] args) {
+		//①
+		var value = 10.0;
+		//②
+		System.out.println("ようこそ、");
+		System.out.println("Javaの世界へ！");
+		//③
+		String str = null;
+		//④
+		int[][] data = new int[5][4]; 
+		//⑤
+		var list = new int[][] {
+				{2,3,5},
+				{1,2},
+				{10,11,12,13},
+		};
+	}
+}
