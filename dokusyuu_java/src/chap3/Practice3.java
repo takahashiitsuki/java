5da42ecfc3c166cfddc4ca8d6dbcc9ec9007e591
takahashiitsuki <itakaha71@gmail.com>
2023-02-28 05:31:37 +0900

    [FInish]chap3

---
+++ dokusyuu_java/src/chap3/Practice3.java
@@ -0,0 +1,10 @@
+package chap3;
+
+public class Practice3 {
+	public static void main(String[] args) {
+		String str = ".java";
+		if(str.endsWith(".java")) {
+			System.out.println("拡張子は.javaです。");
+		}
+	}
+}
