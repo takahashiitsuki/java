b801c1a2a1c7ce358e87f88458a6afe4b294a8d9
takahashiitsuki <itakaha71@gmail.com>
2023-03-02 16:10:07 +0900

    [FInish]Practice2_2

---
+++ dokusyuu_java/src/chap4/Practice2_2.java
@@ -0,0 +1,12 @@
+package chap4;
+
+public class Practice2_2 {
+	public static void main(String[] args) {
+		for(int x = 1; x < 10; x++) {
+			for(int y = 1; y < 10; y++) {
+				System.out.print(x*y + " ");
+			}
+			System.out.println();
+		}
+	}
+}
