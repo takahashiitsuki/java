a2b8519d0b1f479f4082a770c6bae260c778fa7d
takahashiitsuki <itakaha71@gmail.com>
2023-03-04 06:13:16 +0900

    [Fix]Practice3

--- dokusyuu_java/src/chap4/Practice3.java
+++ dokusyuu_java/src/chap4/Practice3.java
@@ -4,11 +4,11 @@ public class Practice3 {
 	public static void main(String[] args) {
 		int sum = 0;
 		for(int i = 100; i <= 200; i++) {
-			if(i / 2 == 0) {
+			if(i % 2 == 0) {
 				continue;
 			}
-			sum = sum + i;
+			sum += i;
 		}
-		System.out.println(sum);
+		System.out.println("奇数の合計値は" + sum);
 	}
 }
