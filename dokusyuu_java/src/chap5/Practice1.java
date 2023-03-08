ab530837bcb06240ba66557ff581355c511110e9
takahashiitsuki <itakaha71@gmail.com>
2023-03-09 06:19:04 +0900

    [Finish]Practice1_②

--- dokusyuu_java/src/chap5/Practice1.java
+++ dokusyuu_java/src/chap5/Practice1.java
@@ -4,5 +4,8 @@ public class Practice1 {
 	public static void main(String[] args) {
 		var str = "となりのきゃくはよくかきくうきゃくだ";
 		System.out.println(str.lastIndexOf("きゃく"));
+		var area = "千葉";
+		var temp = 17.256;
+		System.out.println(String.format("%sの気温は、%.2fです。", area, temp));
 	}
 }
