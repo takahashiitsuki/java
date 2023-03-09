fc7e738a0289dd690284c2e1b1159c1fe9995502
takahashiitsuki <itakaha71@gmail.com>
2023-03-10 01:15:39 +0900

    [Finish]Practice1_③

--- dokusyuu_java/src/chap5/Practice1.java
+++ dokusyuu_java/src/chap5/Practice1.java
@@ -3,9 +3,11 @@ package chap5;
 public class Practice1 {
 	public static void main(String[] args) {
 		var str = "となりのきゃくはよくかきくうきゃくだ";
-		System.out.println(str.lastIndexOf("きゃく"));
+		System.out.println("① " +str.lastIndexOf("きゃく"));
 		var area = "千葉";
 		var temp = 17.256;
-		System.out.println(String.format("%sの気温は、%.2fです。", area, temp));
+		System.out.println(String.format("② %sの気温は、%.2fです。", area, temp));
+		var name = "彼女の名前は花子です。";
+		System.out.println(name.replace("彼女", "妻"));
 	}
 }
