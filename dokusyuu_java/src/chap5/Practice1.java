d40107a7d144378f9072fe5e9d799fa04814515c
takahashiitsuki <itakaha71@gmail.com>
2023-03-11 23:44:02 +0900

    [Finish]Practice1_4

--- dokusyuu_java/src/chap5/Practice1.java
+++ dokusyuu_java/src/chap5/Practice1.java
@@ -1,5 +1,8 @@
 package chap5;
 
+import java.time.Duration;
+import java.time.LocalDateTime;
+
 public class Practice1 {
 	public static void main(String[] args) {
 		var str = "となりのきゃくはよくかきくうきゃくだ";
@@ -8,6 +11,8 @@ public class Practice1 {
 		var temp = 17.256;
 		System.out.println(String.format("② %sの気温は、%.2fです。", area, temp));
 		var name = "彼女の名前は花子です。";
-		System.out.println(name.replace("彼女", "妻"));
+		System.out.println("③ " + name.replace("彼女", "妻"));
+		var time = LocalDateTime.now();
+		System.out.println("④ " + time.plus(Duration.parse("P5DT6H")));
 	}
 }
