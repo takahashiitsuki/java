439e89b714592cffbbd3a4ca383de5ecf6af2935
takahashiitsuki <itakaha71@gmail.com>
2023-03-12 08:22:07 +0900

    [Finish]Practice1

--- dokusyuu_java/src/chap5/Practice1.java
+++ dokusyuu_java/src/chap5/Practice1.java
@@ -1,7 +1,9 @@
 package chap5;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.Period;
 
 public class Practice1 {
 	public static void main(String[] args) {
@@ -14,5 +16,9 @@ public class Practice1 {
 		System.out.println("③ " + name.replace("彼女", "妻"));
 		var time = LocalDateTime.now();
 		System.out.println("④ " + time.plus(Duration.parse("P5DT6H")));
+		var day1 = LocalDate.of(2020, 3, 12);
+		var day2 = LocalDate.of(2020, 11, 5);
+		var period = Period.between(day1, day2);
+		System.out.println("⑤ " + period.getMonths() + "ケ月" + period.getDays() + "日間");
 	}
 }
