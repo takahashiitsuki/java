2ded4f2e94e5e92fa0562c49a253321db84b927e
takahashiitsuki <itakaha71@gmail.com>
2023-03-21 05:59:39 +0900

    [Finish]Practice

---
+++ dokusyuu_java/src/chap6/Practice3.java
@@ -0,0 +1,16 @@
+package chap6;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Practice3 {
+	public static void main(String[] args) {
+		var list = new ArrayList<Integer>(Arrays.asList(1,2,3,4));
+		list.add(100);
+		list.set(2, 30);
+		list.remove(3);
+		for(var i: list) {
+			System.out.println(i);
+		}
+	}
+}
