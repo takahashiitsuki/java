aee8f381463429e0d15b1b81a765057010081416
takahashiitsuki <itakaha71@gmail.com>
2023-03-20 09:26:43 +0900

    [Finish]Practice2

---
+++ dokusyuu_java/src/chap6/Practice2.java
@@ -0,0 +1,23 @@
+package chap6;
+
+import java.util.HashMap;
+
+public class Practice2 {
+	public static void main(String[] args) {
+		var map = new HashMap<String,String>() {
+			{
+			put("cucumber", "キュウリ");
+			put("lettuce", "レタス");
+			put("spinach", "ホウレンソウ");
+			}
+		};
+	
+		map.put("carrot", "ニンジン");
+		map.remove("spinach");
+		map.replace("cucumber", "胡瓜");
+	
+		for(var entry : map.entrySet()) {
+			System.out.println(entry.getKey() + ":" + entry.getValue());
+		}
+	}
+}
