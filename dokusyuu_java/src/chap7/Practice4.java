976e69abaef4dfa112cb244c388b3c7b2968db55
takahashiitsuki <itakaha71@gmail.com>
2023-03-30 23:38:27 +0900

    [FInish]Ptractice7

---
+++ dokusyuu_java/src/chap7/Practice4.java
@@ -0,0 +1,8 @@
+package chap7;
+
+public class Practice4 {
+	public int[] change(int[] value) {
+		value[0] = 100;
+		return value;
+	}
+}
---
+++ dokusyuu_java/src/chap7/Practice4Client.java
@@ -0,0 +1,8 @@
+package chap7;
+
+public class Practice4Client {
+	var value = new int[] {10,20,30};
+	var p = new Practice4();
+	System.out.println(p.change(value)[0]);
+	SYstem.out.println(value[0]);
+}
