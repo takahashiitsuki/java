d7f6af0aa5e7cad0edb7fee903fa311aceb3f7d2
takahashiitsuki <itakaha71@gmail.com>
2023-04-07 19:54:50 +0900

    [Finish]Practice9

---
+++ dokusyuu_java/src/chap9/practice/Weekday.java
@@ -0,0 +1,5 @@
+package chap9.practice;
+
+public class Weekday {
+	public enum Weekday {Monday, Thesday, Wednesday, Thursday, Friday, Saturday, Sunday}
+}
---
+++ dokusyuu_java/src/chap9/practice/WeekdayClient.java
@@ -0,0 +1,7 @@
+package chap9.practice;
+
+public class WeekdayClient {
+	for(var day : Weekday.values()) {
+		System.out.println(day.ordinal() + ":" + day.toString());
+	}
+}
