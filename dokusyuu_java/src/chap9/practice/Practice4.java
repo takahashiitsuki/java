fd1c51707390c12d4a8d8bd45a7deb724c24da44
takahashiitsuki <itakaha71@gmail.com>
2023-04-11 21:53:08 +0900

    [Finish]Practice9

---
+++ dokusyuu_java/src/chap9/practice/Practice4.java
@@ -0,0 +1,28 @@
+package chap9.practice;
+
+public class Practice4 {
+	public class Person{
+		public String firstName;
+		public String last Name;
+		
+		@Override
+		public String toString() {
+			return String.format("Person : %s %s", this.lastName, this.firstName);
+		}
+	}
+	
+	var day = Weekday.valueOf("Monday");
+	System.out.println(day instanceof Weekday);
+	
+	try {
+		
+	}catch(IOException | SQLException e) {}
+	
+	public class Main{
+		private static class Sub{}
+	}
+	
+	public static <T> ArrayList<T> new ArrayList(T data) {
+		return new ArrayList<T>(List.of(data));
+	}
+}
