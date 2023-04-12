caba57d1467aba8bfb21e77f2ce1a60b7237a78e
takahashiitsuki <itakaha71@gmail.com>
2023-04-12 23:58:28 +0900

    [Finish]Practice10_1

---
+++ dokusyuu_java/src/chap10/PCollRemove.java
@@ -0,0 +1,14 @@
+package chap10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class PCollRemove {
+	public static void main() {
+	var list = new ArrayList<String>(
+		Arrays.asList("シュークリーム", "エクレア", "マドレーヌ", "ババロア"));
+	list.removeIf(v -> v.length() < 5);
+	System.out.println(list);
+	System.out.println(list);
+}
+}
