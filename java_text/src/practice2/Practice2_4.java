998889c30323a41b904a1f2bda93ff0b009e476d
takahashiitsuki <itakaha71@gmail.com>
2023-11-19 23:39:33 +0900

    [Fix]text2-4

--- java_text/src/practice2/Practice2_4.java
+++ java_text/src/practice2/Practice2_4.java
@@ -2,9 +2,9 @@ package practice2;
 
 public class Practice2_4 {
 	public static void main(String[] args) {
-		double a = 7;
+		double a = 9;
 		int b = 2;
 		double d = a/b;
-		System.out.println(d);
+		System.out.println(b);
 	}
 }
