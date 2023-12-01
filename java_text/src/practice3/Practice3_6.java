8e8687626a3848045a621f06512f2815fed7375c
takahashiitsuki <itakaha71@gmail.com>
2023-12-01 21:26:11 +0900

    [Fix]text3-6

--- java_text/src/practice3/Practice3_6.java
+++ java_text/src/practice3/Practice3_6.java
@@ -2,7 +2,7 @@ package practice3;
 
 public class Practice3_6 {
 	public static void main(String[] args) {
-		int[] counts = {1, 3, 5, 6, 5, 2};
+		int[] counts = {1, 3, 5, 6, 5, 2, 8};
 		for(int i = 0; i < counts.length; i++) {
 			System.out.print(i + ":");
 			for(int j = 0; j < counts[i]; j++) {
