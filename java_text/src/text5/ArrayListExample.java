891cf97a9e32b12b7bbf9f386e9c7a85ee480420
takahashiitsuki <itakaha71@gmail.com>
2023-12-11 23:50:23 +0900

    [Fix]text

--- java_text/src/text5/ArrayListExample.java
+++ java_text/src/text5/ArrayListExample.java
@@ -6,9 +6,9 @@ public class ArrayListExample {
 	public static void main(String[] args) {
 		ArrayList<String> months = new ArrayList<String>();
 		
-		months.add("January");
-		months.add("February");
-		months.add("march");
+		months.add("January ");
+		months.add("February ");
+		months.add("march ");
 		
 		System.out.println("要素数 " + months.size());
 		for(int i = 0; i < months.size(); i++) {
